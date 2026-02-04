990bcd55c2c3cca973ac893a0cae99c507c265f0
marky <markism1224amarille@gmail.com>
2026-02-04 10:58:59 +0800

    feat(ui): dynamic list for recent buckets

---
+++ Models/Entities/Bucket.cs
@@ -0,0 +1,9 @@
+namespace ScholarFlow.Models.Entities
+{
+    public class Bucket
+    {
+        public string Name { get; set; } = string.Empty;
+        public string FilePath { get; set; } = string.Empty;
+        public string LastModified { get; set; } = string.Empty;
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace ScholarFlow.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ScholarFlow.Models.Entities;
+
+namespace ScholarFlow.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public ObservableCollection<Bucket> RecentProjects { get; set; }
+
+    public MainWindowViewModel()
+    {
+        RecentProjects = new ObservableCollection<Bucket>
+        {
+            new Bucket
+            {
+                Name = "STI",
+                FilePath = "~/somePath",
+                LastModified = "2 Hours Ago",
+            },
+            new Bucket
+            {
+                Name = "Potrero",
+                FilePath = "~/somePath",
+                LastModified = "Some time ago",
+            },
+        };
+    }
 }
