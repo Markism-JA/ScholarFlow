d04f85055de6f44634f1a093d8a629a5c81b0063
marky <markism1224amarille@gmail.com>
2026-02-10 01:08:54 +0800

    setup: use skia for better rendering in linux

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using Avalonia;
-using System;
 
 namespace ScholarFlow;
 
@@ -8,14 +7,18 @@ sealed class Program
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    // [STAThread]
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        return AppBuilder
+            .Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .UseSkia();
+    }
 }
